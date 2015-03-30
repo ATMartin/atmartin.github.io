131c2b0dfcb860cb34cb6475b3a5801cba7897d8
Alex Martin <martin.alex.t+github@gmail.com>
2015-03-30 12:31:11 -0400

    New revision. Testing live.

--- js/app.js
+++ js/app.js
@@ -1,19 +1,25 @@
-/*! viewportSize | Author: Tyson Matanich, 2013 | License: MIT */
-(function(n){n.viewportSize={},n.viewportSize.getHeight=function(){return t("Height")},n.viewportSize.getWidth=function(){return t("Width")};var t=function(t){var f,o=t.toLowerCase(),e=n.document,i=e.documentElement,r,u;return n["inner"+t]===undefined?f=i["client"+t]:n["inner"+t]!=i["client"+t]?(r=e.createElement("body"),r.id="vpw-test-b",r.style.cssText="overflow:scroll",u=e.createElement("div"),u.id="vpw-test-d",u.style.cssText="position:absolute;top:-1000px",u.innerHTML="<style>@media("+o+":"+i["client"+t]+"px){body#vpw-test-b div#vpw-test-d{"+o+":7px!important}}<\/style>",r.appendChild(u),i.insertBefore(r,e.head),f=u["offset"+t]==7?i["client"+t]:n["inner"+t],i.removeChild(r)):f=n["inner"+t],f}})(this);
+$(document).ready(function() {
+ 
+var activePanel;
+var menuHeight = $('.menu-nav').height();
+var menuTop = $('.menu-nav').offset().top;
+  
+$('.toggle').on('click', function() { 
+  activePanel = $(this);
+  activePanel.toggleClass('toggled');
+  $('.toggle').css('opacity', '0');
+  activePanel.next().css({'opacity':'1', 'pointer-events': 'auto'});
+  $('.close-panel').css('opacity', '1');
+});
 
-(function () {
+$('.close-panel').on('click', function(e) {
+  activePanel.toggleClass('toggled');
+  activePanel.next().css({'opacity': '0', 'pointer-events': 'none'});
+  $('.toggle').css('opacity', '1').show();
+  $(this).css('opacity', '0');
+});
 
-  var s = skrollr.init({ render: function(data) { /* console.log(data.curTop); */ } });  
-  var slides = document.getElementsByClassName('homeSlide');
-
-  setTimeout(function() { adjustWindow(); }, 300);
-
-  function adjustWindow() {
-    vpHt = this.viewportSize.getHeight() + 'px';
-    [].forEach.call(slides, function(slide) {
-      slide.style.height = vpHt;
-    });
-    s.refresh(slides);
-  }
-
-})();
+  
+  
+  
+});
---
+++ js/scrollhack.js
@@ -0,0 +1,19 @@
+/*! viewportSize | Author: Tyson Matanich, 2013 | License: MIT */
+(function(n){n.viewportSize={},n.viewportSize.getHeight=function(){return t("Height")},n.viewportSize.getWidth=function(){return t("Width")};var t=function(t){var f,o=t.toLowerCase(),e=n.document,i=e.documentElement,r,u;return n["inner"+t]===undefined?f=i["client"+t]:n["inner"+t]!=i["client"+t]?(r=e.createElement("body"),r.id="vpw-test-b",r.style.cssText="overflow:scroll",u=e.createElement("div"),u.id="vpw-test-d",u.style.cssText="position:absolute;top:-1000px",u.innerHTML="<style>@media("+o+":"+i["client"+t]+"px){body#vpw-test-b div#vpw-test-d{"+o+":7px!important}}<\/style>",r.appendChild(u),i.insertBefore(r,e.head),f=u["offset"+t]==7?i["client"+t]:n["inner"+t],i.removeChild(r)):f=n["inner"+t],f}})(this);
+
+(function () {
+
+  var s = skrollr.init({ render: function(data) { /* console.log(data.curTop); */ } });  
+  var slides = document.getElementsByClassName('homeSlide');
+
+  setTimeout(function() { adjustWindow(); }, 300);
+
+  function adjustWindow() {
+    vpHt = this.viewportSize.getHeight() + 'px';
+    [].forEach.call(slides, function(slide) {
+      slide.style.height = vpHt;
+    });
+    s.refresh(slides);
+  }
+
+})();
